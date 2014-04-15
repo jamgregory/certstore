97b2e61461cb7b5e71bde5c16ec7ecf5a1490355
Andrew Beresford <beezly@beezly.org.uk>
2014-04-15 11:54:54 +0100

    Apply timeout to scans

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,5 +1,6 @@
 require 'socket'
 require 'openssl'
+require 'timeout'
 
 class Service < ActiveRecord::Base
   
@@ -29,20 +30,22 @@ class Service < ActiveRecord::Base
     begin
       Rails.logger.debug "Scanning Certificate for #{hostname}:#{port}"
       socket = TCPSocket.new hostname, port
-      ssl_socket = OpenSSL::SSL::SSLSocket.new socket
-      ssl_socket.connect
-      peer_cert = Certificate.new(keytext: ssl_socket.peer_cert.to_s)
+      Timeout::timeout(10) do 
+        ssl_socket = OpenSSL::SSL::SSLSocket.new socket
+        ssl_socket.connect
+        peer_cert = Certificate.new(keytext: ssl_socket.peer_cert.to_s)
     
-      if not certificate or peer_cert.keytext != certificate.keytext
-        Rails.logger.info "New certificate found for #{hostname}:#{port}: #{peer_cert.serial}"
-        # We found a new certificate - create a new service for it
-        new_service = self.dup
-        new_service.certificate = Certificate.find_or_create_by(keytext: peer_cert.keytext)
-        Rails.logger.debug "Returning new service #{new_service}"
-        return new_service.save
-      else
-        Rails.logger.debug "Unchanged certificate for #{hostname}:#{port}"
-        return false
+        if not certificate or peer_cert.keytext != certificate.keytext
+          Rails.logger.info "New certificate found for #{hostname}:#{port}: #{peer_cert.serial}"
+          # We found a new certificate - create a new service for it
+          new_service = self.dup
+          new_service.certificate = Certificate.find_or_create_by(keytext: peer_cert.keytext)
+          Rails.logger.debug "Returning new service #{new_service}"
+          return new_service.save
+        else
+          Rails.logger.debug "Unchanged certificate for #{hostname}:#{port}"
+          return false
+        end
       end
     rescue => e
       raise "Could not update certificate #{e}"
